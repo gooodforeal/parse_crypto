563c9e56221b4d299a8082d195305debe522d34b
Timothy <88663217+gooodforeal@users.noreply.github.com>
2022-08-17 15:07:59 +0300

    1
    1

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from funcs import *
 # Const
 url = "https://coinmarketcap.com/"
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.100 Safari/537.37"
 }
 
 '''  MAIN   '''
